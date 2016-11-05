74319dd9892bc9211f6276a25aae78f518800c55
TheAssassin <theassassin@users.noreply.github.com>
2016-11-05 17:35:55 +0100

    Add operation 102 (get update status) to debug server

--- server.py
+++ server.py
@@ -43,69 +43,82 @@ def static_files(path=None):
 @app.route("/command.cgi")
 def command_cgi():
     """
-    Almost complete implementation of the FlashAir API endpoint that is
-    relevant for debugging the client application.
+    Implementation of the FlashAir API endpoint that are relevant for
+    debugging the client application.
+
+    FlashAir API documentation:
+    https://www.flashair-developers.com/en/documents/api/commandcgi/
+
+    Implemented operations:
+      - Get file list (op=100)
+      - Get update status (op=102)
     """
 
-    reply = []
+    op = int(request.args["op"])
 
-    path = request.args["DIR"]
-
-    full_path = os.path.abspath(os.path.join(os.curdir, path.lstrip("/")))
-
-    if not os.path.exists(full_path):
-        raise NotFound()
-
-    def is_forbidden(path):
-        return any((part.startswith(".") for part in path.split("/"))) or \
-            any(("SD_WLAN" in p for p in path.split("/")))
-
-    if is_forbidden(path):
-        raise Forbidden()
-
-    for i in os.listdir(full_path):
-        if path != "/" or os.path.isdir(i) and not is_forbidden(i):
-            flags = 0
-
-            filename = os.path.basename(i)
-
-            stat = os.stat(os.path.join(full_path, i))
-
-            if stat.st_mode & 0o040000:
-                # is directory
-                size = 0
-                flags += 1<<4
-
-                # treated as "archive"
-                if filename == "DCIM":
-                    flags += 1<<5
-
-            else:
-                size = stat.st_size
-                flags += 1<<5
-
-            row = [
-                path if path != "/" else "",
-                filename,
-                size,
-                flags,
-                18787,
-                44686
-            ]
-
-            reply.append(",".join((str(i) for i in row)))
-
-    # sort the result
-    reply.sort(key=lambda x: x[1])
-
-    # this line is (whyever) inserted by the original API
-    reply.insert(0, "WLANSD_FILELIST")
-
-    # send as plain text
+    # send responses as plain text
     headers = {"Content-Type": "text/plain"}
 
-    response = app.response_class("\n".join(reply), headers=headers)
-    return response
+    if op == 100:
+        reply = []
+
+        path = request.args["DIR"]
+
+        full_path = os.path.abspath(os.path.join(os.curdir, path.lstrip("/")))
+
+        if not os.path.exists(full_path):
+            raise NotFound()
+
+        def is_forbidden(path):
+            return any((part.startswith(".") for part in path.split("/"))) or \
+                any(("SD_WLAN" in p for p in path.split("/")))
+
+        if is_forbidden(path):
+            raise Forbidden()
+
+        for i in os.listdir(full_path):
+            if path != "/" or os.path.isdir(i) and not is_forbidden(i):
+                flags = 0
+
+                filename = os.path.basename(i)
+
+                stat = os.stat(os.path.join(full_path, i))
+
+                if stat.st_mode & 0o040000:
+                    # is directory
+                    size = 0
+                    flags += 1<<4
+
+                    # treated as "archive"
+                    if filename == "DCIM":
+                        flags += 1<<5
+
+                else:
+                    size = stat.st_size
+                    flags += 1<<5
+
+                row = [
+                    path if path != "/" else "",
+                    filename,
+                    size,
+                    flags,
+                    18787,
+                    44686
+                ]
+
+                reply.append(",".join((str(i) for i in row)))
+
+        # sort the result
+        reply.sort(key=lambda x: x[1])
+
+        # this line is (whyever) inserted by the original API
+        reply.insert(0, "WLANSD_FILELIST")
+
+        return app.response_class("\n".join(reply), headers=headers)
+
+    if op == 102:
+        # always returns 1 to indicate an update event
+        return app.response_class("1\n", headers=headers)
 
 
 # run debug server in PyCharm compatible mode (aka disabled reloader)
